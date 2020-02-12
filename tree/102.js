d56b884f3157edbf9a5ec8a9dd80773d6ebb604a
Michael <michael.gourion@qeezz.com>
2020-02-12 23:42:54 +0200

    102

--- tree/102.js
+++ tree/102.js
@@ -9,18 +9,47 @@ function TreeNode(val) {
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    
+    let result =[];
+    let fifo = [];
+    let subResult=[];
+
+    if( root == null ){
+        return result;
+    }  
+    fifo.unshift( root ) ;
+
+    while( fifo.length > 0 ){
+        let level_size = fifo.length;
+        for( let i = 0 ; i < level_size ; i++ ){
+            let node = fifo.pop(); 
+            subResult.push( node.val ) ;
+            if( node.left != null ){
+                fifo.unshift( node.left ) ;
+            }
+            if( node.right != null ){
+                fifo.unshift( node.right ) ; 
+            }
+        }
+        result.push( subResult ) ;
+        subResult = [];
+    }
+
+    return result;
 };
 
 
 // 3,9,20,null,null,15,7]
 function getTree(){
-    let node = new TreeNode(3) ;
-    node.left = new TreeNode(9);
-    node.right = new TreeNode(20) ;
-
-    node = node.right;
-    node.left = new TreeNode(15) ;
-    node.right = new TreeNode(7) ;
+    let root = new TreeNode(1) ;
+    root.left = new TreeNode(2);
+    let node = root.left;
+    node.left = new TreeNode(3);
+    node = node.left;
+    node.left = new TreeNode(4);
+    node = node.left;
+    node.left = new TreeNode(5);
+    return root;
 }
 
+let root = getTree();
+console.log( levelOrder(root) ) ;
